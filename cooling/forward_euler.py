6a0f6fd9813ff7e580798cb0d9792114fd949cb3
Pavel Chernitsyn <suiccidesilence@gmail.com>
2021-06-14 21:33:51 +0300

    add status msg. Now FE is working!

--- cooling/forward_euler.py
+++ cooling/forward_euler.py
@@ -3,22 +3,24 @@ import numpy as np
 import my_func as mf
 
 class ForwardEuler:
-    def __init__(self, _h = 0.2, _x = np.array([1.0, 200.0]), _yinit = np.array([80.0])):
+    def __init__(self, _h = 0.2, _x = 200, _y0 = 100, env_temp_ = 50):
         self.h = _h
-        self.x = _x
-        self.y0 = np.array([80.0])
+        self.x = np.array([0.0, _x])
+        self.y0 = np.array([_y0])
+        self.start_temp = _y0
+        self.env_temp = env_temp_
         self.f = open('tmp.txt', 'w')
 
     def __del__(self):
         self.f.close()
         pass
 
-    def main_FE(self, y0, x_range, h):
-        y_len = len(y0)
-        x_len = int((x_range[-1] - x_range[0])/h)
+    def main_FE(self):
+        y_len = len(self.y0)
+        x_len = int((self.x[-1] - self.x[0])/self.h)
         
-        x = x_range[0]
-        y = y0
+        x = self.x[0]
+        y = self.y0
         
         self.f.write(str(x) + ' ')
 
@@ -26,12 +28,12 @@ class ForwardEuler:
         y_res = np.append(y_res, y)
 
         for i in range(x_len):
-            F_x_t = mf.myFunc(y)
+            F_x_t = mf.myFunc(y, self.env_temp)
             
             for j in range(y_len):
-                y[j] = y[j] + h*F_x_t[j]
+                y[j] = y[j] + self.h*F_x_t[j]
                 
-            x += h
+            x += self.h
             self.f.write(str(x) + ' ')
 
             for r in range(len(y)):
@@ -42,18 +44,18 @@ class ForwardEuler:
         return y_res
 
     def execute(self):
-        ys = self.main_FE(self.y0, self.x, self.h)
+        ys = self.main_FE()
         for index in ys:
             self.f.write(str(index) + ' ')
         self.f.write('\n')
 
-        t = np.arange(0, 200, 1)
+        t = np.arange(0, self.x[-1], 1)
         for index in t:
             self.f.write(str(index) + ' ')
         self.f.write('\n')
 
         for i in t:
-            y_math_res = 40 + 40 * np.e**(-(np.log(2) * i/20))
+            y_math_res = self.env_temp + (self.start_temp - self.env_temp) * np.e**(-(np.log(2) * i/25))
             self.f.write(str(y_math_res) + ' ')
         self.f.write('\n')
 
--- cooling/gui.py
+++ cooling/gui.py
@@ -62,61 +62,101 @@ class MainApplication(ttk.Frame):
         notes.add(page, text='Picture')
 
 
-        plot = Plotter(page)
+        self.plot = Plotter(page)
 
         input_frame = ttk.Frame(self)
         input_frame.grid(column=1, row=0, sticky='nsew')
 
         label_top_temp = ttk.Label(input_frame)
-        slider_top_temp = ttk.Scale(input_frame, from_ = 20, to_ = 200,
+        self.slider_top_temp = ttk.Scale(input_frame, from_ = 20, to_ = 200,
                             command=lambda x:
-                            label_top_temp.config(text = "Top Temp = " + str(int(slider_top_temp.get()))))
-        slider_top_temp.set(100)
-        label_top_temp.config(text = "Top Temp = " + str(int(slider_top_temp.get())))
+                            label_top_temp.config(text = "Top Temp = " + str(int(self.slider_top_temp.get()))))
+        self.slider_top_temp.set(100)
+        label_top_temp.config(text = "Top Temp = " + str(int(self.slider_top_temp.get())))
 
         label_env_temp = ttk.Label(input_frame)
-        slider_env_temp = ttk.Scale(input_frame, from_ = 0, to_ = 180,
+        self.slider_env_temp = ttk.Scale(input_frame, from_ = 0, to_ = 180,
                             command=lambda x:
-                            label_env_temp.config(text = "Enviroment Temp = " + str(int(slider_env_temp.get()))))
-        slider_env_temp.set(50)
-        label_env_temp.config(text = "Enviroment Temp = " + str(int(slider_env_temp.get())))
+                            label_env_temp.config(text = "Enviroment Temp = " + str(int(self.slider_env_temp.get()))))
+        self.slider_env_temp.set(50)
+        label_env_temp.config(text = "Enviroment Temp = " + str(int(self.slider_env_temp.get())))
 
         label_time = ttk.Label(input_frame)
-        slider_time = ttk.Scale(input_frame, from_ = 20, to_ = 1000,
+        self.slider_time = ttk.Scale(input_frame, from_ = 20, to_ = 1000,
                             command=lambda x:
-                            label_time.config(text = "Time = " + str(int(slider_time.get()))))
-        slider_time.set(200)
-        label_time.config(text = "Time = " + str(int(slider_time.get())))
+                            label_time.config(text = "Time = " + str(int(self.slider_time.get()))))
+        self.slider_time.set(200)
+        label_time.config(text = "Time = " + str(int(self.slider_time.get())))
 
-        button_BE = ttk.Button(input_frame, text='Backward Euler', 
-                            command=lambda: 
-                            plot.draw_lists(backward_euler.BackwardEuler(
-                                0.2, int(slider_time.get()), slider_top_temp.get(), slider_env_temp.get()).execute()))
-        button_FE = ttk.Button(input_frame, text='Forward Euler', 
-                            command=lambda: 
-                            plot.draw_lists(forward_euler.ForwardEuler().execute()))
+        self.label_err_msg = ttk.Label(input_frame)
+        self.label_err_msg.config(text = "")
+
+        button_BE = ttk.Button(input_frame, text='Backward Euler', command = self.button_BE_clicked)
+                            
+        button_FE = ttk.Button(input_frame, text='Forward Euler', command = self.button_FE_clicked)
+        
         button_RK = ttk.Button(input_frame, text='Runge Kutt', 
                             command=lambda: 
-                            plot.draw_lists(runge_kutta.Runge_Kutt().execute()))
+                            self.plot.draw_lists(runge_kutta.Runge_Kutt().execute()))
         button_H = ttk.Button(input_frame, text='Heun', 
                             command=lambda: 
-                            plot.draw_lists(heun.Heun().execute()))
+                            self.plot.draw_lists(heun.Heun().execute()))
         button_ADM = ttk.Button(input_frame, text='Adams-Bashforth-Moulton', 
                             command=lambda: 
-                            plot.draw_lists(adams_bashforth_moulton.ABM().execute()))
+                            self.plot.draw_lists(adams_bashforth_moulton.ABM().execute()))
                             
-        button_BE.grid(column=0, row=5, columnspan=2, sticky='ew')
-        button_FE.grid(column=0, row=7, columnspan=2, sticky='ew')
-        button_RK.grid(column=0, row=9, columnspan=2, sticky='ew')
-        button_H.grid(column=0, row=11, columnspan=2, sticky='ew')
-        button_ADM.grid(column=0, row=13, columnspan=2, sticky='ew')
-        label_top_temp.grid(column=0, row=15, columnspan=2, sticky='ew')
-        slider_top_temp.grid(column=0, row=17, columnspan=2, sticky='ew')
-        label_env_temp.grid(column=0, row=19, columnspan=2, sticky='ew')
-        slider_env_temp.grid(column=0, row=21, columnspan=2, sticky='ew')
-        label_time.grid(column=0, row=23, columnspan=2, sticky='ew')
-        slider_time.grid(column=0, row=25, columnspan=2, sticky='ew')
+        button_BE.grid(column=0, row=0, columnspan=2, sticky='ew')
+        button_FE.grid(column=0, row=1, columnspan=2, sticky='ew')
+        button_RK.grid(column=0, row=2, columnspan=2, sticky='ew')
+        button_H.grid(column=0, row=3, columnspan=2, sticky='ew')
+        button_ADM.grid(column=0, row=4, columnspan=2, sticky='ew')
+        label_top_temp.grid(column=0, row=5, columnspan=2, sticky='ew')
+        self.slider_top_temp.grid(column=0, row=6, columnspan=2, sticky='ew')
+        label_env_temp.grid(column=0, row=7, columnspan=2, sticky='ew')
+        self.slider_env_temp.grid(column=0, row=8, columnspan=2, sticky='ew')
+        label_time.grid(column=0, row=9, columnspan=2, sticky='ew')
+        self.slider_time.grid(column=0, row=10, columnspan=2, sticky='ew')
+        self.label_err_msg.grid(column=0, row=12, columnspan=2, sticky='ew')
 
+    def button_BE_clicked(self):
+        if (self.check_sliders()):
+            self.plot.draw_lists(backward_euler.BackwardEuler(
+                0.2, int(self.slider_time.get()), self.slider_top_temp.get(), self.slider_env_temp.get()
+                ).execute())
+
+    def button_FE_clicked(self):
+        if (self.check_sliders()):
+            self.plot.draw_lists(forward_euler.ForwardEuler(
+                0.2, int(self.slider_time.get()), self.slider_top_temp.get(), self.slider_env_temp.get()
+                ).execute())
+
+    def button_RK_clicked(self):
+        if (self.check_sliders()):
+            self.plot.draw_lists(backward_euler.BackwardEuler(
+                0.2, int(self.slider_time.get()), self.slider_top_temp.get(), self.slider_env_temp.get()
+                ).execute())
+
+    def button_H_clicked(self):
+        if (self.check_sliders()):
+            self.plot.draw_lists(backward_euler.BackwardEuler(
+                0.2, int(self.slider_time.get()), self.slider_top_temp.get(), self.slider_env_temp.get()
+                ).execute())
+
+    def button_ADM_clicked(self):
+        if (self.check_sliders()):
+            self.plot.draw_lists(backward_euler.BackwardEuler(
+                0.2, int(self.slider_time.get()), self.slider_top_temp.get(), self.slider_env_temp.get()
+                ).execute())
+
+    def check_sliders(self):
+        # print("hi")
+        if (self.slider_top_temp.get() <= self.slider_env_temp.get()):
+            self.label_err_msg.config(text = "\n\nERROR!\nTop Temp <= Env Temp")
+            return False
+        else:
+            self.label_err_msg.config(text = "")
+            return True
+    
     def __del__(self):
         os.remove('tmp.txt')
         pass
